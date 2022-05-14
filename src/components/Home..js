28a3a8096ed98444529ca6b65b0f06161ea716d2
GlauciaBierwagen <galsilv@alumni.usp.br>
2022-05-14 13:51:29 +0200

    Adding components

---
+++ src/components/Home..js
@@ -0,0 +1,13 @@
+import About from "./About";
+
+const Home = () => {
+    return ( 
+      <div className="container">
+          <h1> Home</h1>
+          <p> This is Home Page</p>
+      </div>
+    );  
+};
+
+export default Home;
+
